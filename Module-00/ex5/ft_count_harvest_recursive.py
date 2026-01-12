1f242694e4232d23d714d1442594ae1021c1f613
Gaspard Cabecas-Segura <gcabecas@z4r8p4.42lyon.fr>
2026-01-12 10:37:46 +0100

    push

--- Module-00/ex5/ft_count_harvest_recursive.py
+++ Module-00/ex5/ft_count_harvest_recursive.py
@@ -1,11 +1,11 @@
-def ft_count_harvest_recursive():
-    days = int(input("Days until harvest: "))
-
-    def count(day):
-        if day > days:
-            print("Harvest time!")
-            return
-        print("Day ", day)
-        count(day + 1)
-
-    count(1)
+def ft_count_harvest_recursive(days_until=0, first_day=0):
+    if first_day == 0:
+        days_until = int(input("Days until harvest: "))
+        first_day += 1
+    print(f"Day {first_day}")
+    if days_until > first_day:
+        ft_count_harvest_recursive(days_until, first_day + 1)
+    if first_day == days_until:
+        print("Harvest time!")
+    elif days_until == 0:
+        print("Harvest time!")
