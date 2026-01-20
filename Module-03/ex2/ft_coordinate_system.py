16dc5532f237ab0c4df239a2b4e22d3b2383b6d1
Gaspard Cabecas-Segura <gcabecas@z4r11p6.42lyon.fr>
2026-01-20 15:18:15 +0100

    push

--- Module-03/ex2/ft_coordinate_system.py
+++ Module-03/ex2/ft_coordinate_system.py
@@ -1,6 +1,37 @@
 import sys
 import math
 
-if __name__ == "__main__":
+
+def calc() -> None:
     print("=== Game Coordinate System ===")
-    
+    argc: int = len(sys.argv)
+    if argc != 2:
+        print("wrong number of args")
+    else:
+        try:
+            cords: tuple = sys.argv[1].split(",")
+            if len(cords) != 3:
+                raise ValueError(
+                    "Wrong number of cords"
+                )
+        except ValueError as err:
+            print(f"Error: {err}")
+            return
+
+        (x, y, z) = cords
+        try:
+            dist = float(math.sqrt(
+                (0 - int(x))**2 + (0 - int(y))**2 + (0 - int(z))**2))
+        except ValueError as err:
+            print(f"Error: {err}")
+            return
+
+        print(
+            f"Distance between (0, 0, 0) and ("
+            f"{x}, "
+            f"{y}, "
+            f"{z}): {dist:.2f}")
+
+
+if __name__ == "__main__":
+    calc()
